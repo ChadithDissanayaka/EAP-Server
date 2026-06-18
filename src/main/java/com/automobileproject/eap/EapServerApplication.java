45bccd001454f5ce532937d48e8105adbf271c96
E. J. Chadith Dissanayaka <124421386+ChadithDissanayaka@users.noreply.github.com>
2026-06-18 21:58:40 +0530

    Initial commit

---
+++ src/main/java/com/automobileproject/eap/EapServerApplication.java
@@ -0,0 +1,13 @@
+package com.automobileproject.eap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EapServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EapServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/automobileproject/eap/EapServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.automobileproject.eap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EapServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
