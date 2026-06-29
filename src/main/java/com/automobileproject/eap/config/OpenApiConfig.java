0285a602c9236ecd789a9bb1643917f458ab2df2
E. J. Chadith Dissanayaka <chadithdissanayaka@gmail.com>
2026-06-29 18:10:26 +0530

    Fixed issue not working swagger document.

--- src/main/java/com/automobileproject/eap/config/OpenApiConfig.java
+++ src/main/java/com/automobileproject/eap/config/OpenApiConfig.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,6 +22,7 @@ public class OpenApiConfig {
                         .version("1.0")
                         .description("Backend API for the Automobile Service Management System")
                         .contact(new Contact().name("EAP Team")))
+                .addSecurityItem(new SecurityRequirement().addList(BEARER_AUTH))
                 .components(new Components()
                         .addSecuritySchemes(BEARER_AUTH, new SecurityScheme()
                                 .name(BEARER_AUTH)
